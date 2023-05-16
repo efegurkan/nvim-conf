df09be1628349a736e40e20547aff5654b52fea5
Efe Gürkan YALAMAN <efeyalaman@gmail.com>
2023-05-16 15:14:06 +0200

    Update tree settings

--- lua/tree-settings.lua
+++ lua/tree-settings.lua
@@ -2,4 +2,33 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-
+require("neo-tree").setup({
+  window = {
+    mappings = {
+      ["z"] = "noop",
+      ["l"] = "noop"
+    }
+  },
+  filesystem = {
+    window = {
+      mappings = {
+        -- disable fuzzy finder
+        ["/"] = "noop"
+      }
+    }
+  },
+  git_status = {
+    window = {
+      mappings = {
+        -- disable git stuff, as it has commit and push kind of nonsense mapped to gg
+        ["A"] = "noop",
+        ["gu"] = "noop",
+        ["ga"] = "noop",
+        ["gr"] = "noop",
+        ["gc"] = "noop",
+        ["gp"] = "noop",
+        ["gg"] = "noop",
+      }
+    }
+  }
+})
